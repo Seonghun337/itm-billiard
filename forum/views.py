03e4eb0a06b0629aa54d596cdc5a28161c56a418
Seonghun337 <sung2687@gmail.com>
2021-07-14 03:08:24 +0900

    create post_detail using CBV

--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     template_name = 'forum/index.html'
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
 # def index(request):
