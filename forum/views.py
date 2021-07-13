559f0733458dd6e9b4681f497f859549ff64cda2
Seonghun337 <sung2687@gmail.com>
2021-07-14 00:56:25 +0900

    change forum from FBV to CBV

--- forum/views.py
+++ forum/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
+
+class PostList(ListView):
+    model = Post
+    template_name = 'forum/index.html'
+    ordering = '-pk'
+
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
 
 
-    return render(
-        request,
-        'forum/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#     return render(
+#         request,
+#         'forum/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'forum/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#     return render(
+#         request,
+#         'forum/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
